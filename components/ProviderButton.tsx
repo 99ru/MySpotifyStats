12d2e97783d6a8ce32ddb5b393546ca349000fa3
99ru <yan.99ru@gmail.com>
2023-07-31 19:47:04 +0200

    error fix?

--- components/ProviderButton.tsx
+++ components/ProviderButton.tsx
@@ -15,7 +15,6 @@ export default function ProviderButton({
 		<button
 			key={name}
 			className="rounded-full bg-green-500 px-4 py-2 text-xl font-semibold text-black shadow-lg shadow-gray-800 transition duration-300 hover:bg-white hover:text-black"
-			/* eslint-disable-next-line @typescript-eslint/no-misused-promises */
 			onClick={async (): Promise<void> => void (await signIn(id, { callbackUrl: callback }))}>
 			Sign in with {name}
 		</button>
